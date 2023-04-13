81c7882cd1143e65941094ea14ef036d0904d1cd
fishphobiagg <dlauddn6211@icloud.com>
2023-04-14 01:11:09 +0900

    navbar

--- SSAF_Possible/frontend/ssaf_possible/src/App.js
+++ SSAF_Possible/frontend/ssaf_possible/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Nav, Navbar, NavDropdown, Container } from 'react-bootstrap';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <Navbar bg="light" variant="light" fixed='top'>
+        <Container>
+          <Navbar.Brand href="#home">쌒!가능</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">기술블로그</Nav.Link>
+            <Nav.Link href="#features">채용공고</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
+
   );
 }
+
 
 export default App;
