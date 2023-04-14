069bc37019a11797dd310943fa6f2f96ef159718
Fishphobiagg <dlauddn6211@icloud.com>
2023-04-14 10:45:22 +0900

    connect

--- SSAF_Possible/frontend/ssaf_possible/src/App.js
+++ SSAF_Possible/frontend/ssaf_possible/src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
 import { Button, Nav, Navbar, NavDropdown, Container } from 'react-bootstrap';
-
+import Article from './routes/Article.js'
 
 function App() {
+  const url = 'http://127.0.0.1:8000/api/techblog/articles/';
+  fetch(url).then((res:)=>resizeBy.json()).then(data=>console.log(src))
   return (
     <div className="App">
-            <Navbar bg="light" variant="light" fixed='top'>
+      <div>
+      <Navbar bg="light" variant="light" fixed='top'>
         <Container>
-          <Navbar.Brand href="#home">쌒!가능</Navbar.Brand>
+          <Navbar.Brand href="#home" className='navfont'>쌒!가능</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">기술블로그</Nav.Link>
             <Nav.Link href="#features">채용공고</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+      </div>
+      <Article>1234s</Article>
     </div>
 
   );
---
+++ SSAF_Possible/frontend/ssaf_possible/src/routes/Article.js
@@ -0,0 +1,7 @@
+function Article() {
+  return (
+<h4>Article</h4>
+  )
+}
+
+export default Article;
