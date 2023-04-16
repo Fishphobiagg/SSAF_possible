49e19cb959678d3745dd88ba9f5e835265a1ad0d
fishphobiagg <dlauddn6211@icloud.com>
2023-04-16 15:39:49 +0900

    LINE crawling

--- SSAF_Possible/backend/articles/utils/crawler.py
+++ SSAF_Possible/backend/articles/utils/crawler.py
@@ -17,13 +17,12 @@ def crawl_data():
         soup = BeautifulSoup(res.text, 'html.parser')
         for k in soup.find_all('article')[3:]:
             article_id = k['class'][2].split('-')[1]
-            title = k.find('h3').get_text()
+            title = k.find('h3').get_text().strip()
             link = k.find('h3').find('a')['href']
-            author = k.find('span', attrs={'class':'elementor-post-author'}).get_text()
+            author = k.find('span', attrs={'class':'elementor-post-author'}).get_text().strip()
             content = k.find('div', attrs={'class':'elementor-post__excerpt'}).find('p').get_text()
-            published_date = k.find('span', attrs={'class':'elementor-post-date'}).get_text()
-            print(link, type(link))
-            if not Article.objects.filter(title=title, article_id=article_id, ent_name='KAKAO').exists():
+            published_date = k.find('span', attrs={'class':'elementor-post-date'}).get_text().strip()
+            if not Article.objects.filter(article_id=article_id, ent_name='KAKAO').exists():
                 # Create a new MyModel instance and save it to the database
                 article = Article.objects.create(
                     title=title,
@@ -36,10 +35,35 @@ def crawl_data():
                 )
                 article.save()
     for i in range(1, 6):
-        url = 'https://engineering.linecorp.com/ko/blog'
+        if i == 1:
+            url = 'https://engineering.linecorp.com/ko/blog'
+        else:
+            url = 'https://engineering.linecorp.com/ko/blog' + f'/page/{i}'
         res = requests.get(url)
         res.raise_for_status()
         soup = BeautifulSoup(res.text, 'html.parser')
+        print(url)
+        for k in soup.find_all('li', attrs={'class':'post_list_item'}):
+            title = k.find('h2').find('a').get_text()
+            link = k.find('h2').find('a')['href']
+            article_id = title
+            text_area = k.find('div', attrs={'class':'text_area'})
+            author = text_area.find('a', attrs={'class':'text_name'}).get_text()
+            content = k.find('span', attrs={'class':'text'}).get_text()
+            published_date = text_area.find('span').get_text()
+            print(title, link, content,)
+        if not Article.objects.filter(article_id=article_id, ent_name='LINE').exists():
+                # Create a new MyModel instance and save it to the database
+                article = Article.objects.create(
+                    title=title,
+                    article_id=article_id,
+                    ent_name='LINE',
+                    published_date=published_date,
+                    link=link,
+                    author=author,
+                    content=content,
+                )
+                # article.save()
     # elif enterprise == 'woowahan':
     #     url = 'https://techblog.woowahan.com/'
     #     res = requests.get(url)
