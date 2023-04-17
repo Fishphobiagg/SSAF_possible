4b1d51f2ffdbcda24a2804dd2f08d8c97eeaec52
fishphobiagg <dlauddn6211@icloud.com>
2023-04-18 01:43:27 +0900

    pagination

--- SSAF_Possible/frontend/ssaf_possible/src/routes/Article.js
+++ SSAF_Possible/frontend/ssaf_possible/src/routes/Article.js
@@ -1,33 +1,56 @@
 import React, {useState, useEffect} from "react";
 import './Article.css';
+import { Pagination, Container, Row, Col} from "react-bootstrap";
+
 
 function Article() {
-
+  const basic_url = 'http://127.0.0.1:8000/api/techblog/articles/?page='
   const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [perPage] = useState(10);
+  const [url, setUrl] = useState(basic_url+page)
+
   useEffect(()=>{
-    fetch('http://127.0.0.1:8000/api/techblog/articles/')
+    fetch(url)
     .then(response => response.json())
     .then(data => setData(data));
-  }, []);
-
-console.log(data)
+  }, [page]);
+  let [maxpage, setMaxpage] = useState(parseInt(data.count/10));
 
   return (
-    <div>{data.map((a, i) => {
-      return(
-        <div className="content-field">
-          <hr/>
-          <h4 className="title"><a href={a.link}>{a.title}</a>
-          </h4>
-          <p>{a.author} {a.published_date}</p>
-          <div>{a.content}</div>
-        </div>
+    <div>
+      <Container>
+        <Row>
+          {data.results.map(function(a, i){
+            
+            return(
+              <Col>
+                <h4>{a.title}</h4>
+              </Col>
+            )
+          })}
+        </Row>
+      </Container>
+      <Pagination></Pagination>
       
-      
-      )
-
-    })}</div>
+    </div>
   );
+}
+
+function Content(props) {
+
+  console.log(props.data.results);
+  <Col>
+    {props.data.results.map(function(a, i){
+
+      console.log(a)
+      return (
+        <h4>{a.title}</h4>
+      )
+    })
+      
+    }
+  </Col>
 }
 
 export default Article;
