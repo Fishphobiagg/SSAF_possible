a9e66148efaee3880b7af5765cf8464eed9a2303
Fishphobiagg <dlauddn6211@icloud.com>
2023-04-25 17:50:49 +0900

    1234

--- SSAF_Possible/backend/articles/serializers.py
+++ SSAF_Possible/backend/articles/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Article
 
 class ArticleListSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Article
         fields = ('title', 'ent_name', 'author', 'content', 'published_date', 'link', 'id')
