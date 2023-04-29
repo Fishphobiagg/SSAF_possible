929f44e0246739c2bd98b60d98b41392cfc5d3b6
fishphobiagg <dlauddn6211@icloud.com>
2023-04-29 16:07:37 +0900

    frontend scrap feature

--- SSAF_Possible/frontend/ssaf_possible/src/routes/Scrap.js
+++ SSAF_Possible/frontend/ssaf_possible/src/routes/Scrap.js
@@ -1,4 +1,9 @@
+import { useState } from "react";
 function Scrap(){
+
+    const scrapListApi = 'http://127.0.0.1:8000/api/techblog/scraps/'
+    const scrapApi = (article_pk) => {'http://127.0.0.1:8000/api/techblog/scraps/' + article_pk}
+    const [scrapData, setScrapData] = useState()
     return(
         <div>
             <h2>Scraps</h2>
