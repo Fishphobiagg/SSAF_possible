848c5ce50d5872ef3c9fce27f6b5a889efffdf7b
fishphobiagg <dlauddn6211@icloud.com>
2023-04-30 19:52:52 +0900

    frontend login/logout feature

---
+++ SSAF_Possible/frontend/ssaf_possible/src/routes/Info.js
@@ -0,0 +1,17 @@
+import './Info.css';
+
+function Info(){
+    return (
+        <div className="content">
+            <h1>How to Use</h1>
+            <span className='text'>
+            <p>아이디 : 이름</p>
+            <p>초기 비밀번호 : 1111</p>
+            <p>초기 이메일 : 1111</p>
+            <p>추가로 원하시는 기술블로그나 채용정보 있으시면 저한테 말씀해주세요!</p>
+            </span>
+        </div>
+    )
+}
+
+export default Info;
